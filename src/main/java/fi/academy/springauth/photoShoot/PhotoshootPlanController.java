a81f68604d58c6d05bab00b45ee8516c984eac74
iirissetala <iirissetala@hotmail.com>
2019-08-20 15:51:08 +0300

    put ja delete by id lisätty suunnitelmille, plus photoshootplanservice, kommentit, testattu että kaikki toimii

--- src/main/java/fi/academy/springauth/photoShoot/PhotoshootPlanController.java
+++ src/main/java/fi/academy/springauth/photoShoot/PhotoshootPlanController.java
@@ -1,22 +1,67 @@
 package fi.academy.springauth.photoShoot;
 
+import fi.academy.springauth.appUser.AppUserEntity;
+import fi.academy.springauth.appUser.AppUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/plans")
 public class PhotoshootPlanController {
 
     @Autowired
+    private AppUserRepository appUserRepository;
+
+    @Autowired
     private PhotoshootPlanRepository photoshootPlanRepository;
 
-    @GetMapping("")
+    @Autowired
+    private PhotoshootPlanService photoshootPlanService;
+
+    /*@GetMapping("")
     public List<PhotoshootPlanEntity> getAllPlan() {
         return photoshootPlanRepository.findAll();
+    }*/
+
+    /**
+     * Hakee kaikki kirjautuneena olevan käyttäjän luomat suunnitelmat.
+     * Kirjautuneena oleva käyttäjä saadaan Principalin avulla, palautetaan ko käyttäjän suunnitelmalista
+     */
+    @GetMapping("")
+    public List<PhotoshootPlanEntity> findbyCreator(Principal user){
+        Optional<AppUserEntity> appuser = appUserRepository.findByUsername(user.getName());
+        return photoshootPlanRepository.findByCreator(appuser);
+    }
+
+    /**
+     * Luo uuden suunnitelman
+     * Hakee kirjautuneen käyttäjän Principalin avulla ja asettaa käyttäjän suunnitelman luojaksi.
+     */
+    @PostMapping("")
+    public void createPlan (@RequestBody PhotoshootPlanEntity plan, Principal user){
+        Optional<AppUserEntity> appuser = appUserRepository.findByUsername(user.getName());
+        plan.setCreator(appuser.get());
+        photoshootPlanRepository.save(plan);
+    }
+    /**
+     * Poistaa suunnitelman. Ks PhotoshootPlanService.
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletePlan(@PathVariable long id, Principal user) {
+        return photoshootPlanService.deletePlan(id, user);
+    }
+    /**
+     * Muokkaa luotua suunnitelmaa. Ks PhotoshootPlanService.
+     */
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editPlan(@PathVariable long id, @RequestBody PhotoshootPlanEntity plan, Principal user) {
+        return photoshootPlanService.editPlan(id, plan, user);
     }
 
 }
--- src/main/java/fi/academy/springauth/photoShoot/PhotoshootPlanRepository.java
+++ src/main/java/fi/academy/springauth/photoShoot/PhotoshootPlanRepository.java
@@ -3,8 +3,12 @@ package fi.academy.springauth.photoShoot;
 import fi.academy.springauth.appUser.AppUserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PhotoshootPlanRepository extends JpaRepository<PhotoshootPlanEntity, Long> {
 
-    PhotoshootPlanEntity findByCreator(AppUserEntity user);
+    List<PhotoshootPlanEntity> findByCreator(Optional<AppUserEntity> user);
     PhotoshootPlanEntity findByCreator_Username(String username);
+    //PhotoshootPlanRepository findById(long id);
 }
